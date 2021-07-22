332810e74db5b5f74c14d483a9d504b1cdabd834
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-07-22 11:53:50 -0400

    Setting up code grade

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
         + If a plane takes off, its `isFlying` property gets set to true.
         + If a plane lands, its `isFlying` property gets set to false.
 */
-
+// setting up CodeGrade
 // EXAMPLE SOLUTION CODE:
 class Airplane {
   constructor(name) {
